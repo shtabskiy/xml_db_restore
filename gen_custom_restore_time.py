1369b202c2eca49de0e51fd6155e1238a60a243a
Alexey Shtabskiy <41116895+shtabskiy@users.noreply.github.com>
2022-04-09 22:42:11 +0300

    Create gen_custom_restore_time.py

---
+++ gen_custom_restore_time.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# https://t.me/ShtabskiyAD
+
+from datetime import datetime, timedelta
+import xml.etree.ElementTree as ET
+
+tdate = datetime.today() - timedelta(hours=10, minutes=0)
+recover_time = tdate.strftime('%Y-%m-%d %H:%M:%S')
+
+
+tree = ET.parse('to_time.xml')
+root = tree.getroot()
+
+for i in root.iter('toTimeValue'):
+	new_i = str(recover_time)
+	i.text = str(new_i)
+
+	for a in root.iter('timeValue'):
+		new_a = str(recover_time)
+		a.text = str(new_a)
+
+tree.write('output.xml', short_empty_elements=False, encoding='UTF-8', xml_declaration=True)
